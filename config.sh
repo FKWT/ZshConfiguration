e857a8d710de98c662442ed47b7d7875eecbda03
smyrcu <bl.szadkowski@gmail.com>
2025-12-18 15:27:30 +0100

    .

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
-sudo apt install -y zsh
+#!/bin/bash
+set -e
+
+sudo apt install -y zsh git curl wget
 RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 wget -O ~/.p10k.zsh https://raw.githubusercontent.com/FKWT/ZshConfiguration/refs/heads/master/p10k.server
 wget -O ~/.zshrc https://raw.githubusercontent.com/FKWT/ZshConfiguration/refs/heads/master/zshrc.server
-chsh -s $(which zsh)
+sudo chsh -s "$(which zsh)" "$USER"
